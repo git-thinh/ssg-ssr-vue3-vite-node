da713012c0bf1aa75082a55c757d62cceaecf126
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2022-09-19 06:33:58 +0700

    34234

--- v02/src/Page.vue
+++ v02/src/Page.vue
@@ -1,10 +1,11 @@
 <template>
-	<h1>Page All</h1>
+	<h1>Page All: pageKey = {{pageKey}}</h1>
 	<component :is="c"></component>
-	<Runtime :name="'rtTestRuntime'"/>
+	<Runtime :name="'rtTestRuntime'" />
 </template>
 
 <script setup>
+	import Runtime from '@/runtime'
 	import {
 		h,
 		compile,
@@ -13,12 +14,23 @@
 		resolveComponent,
 		defineAsyncComponent
 	} from 'vue'
-	import Runtime from '@/runtime'
+	import {
+		useRouter,
+		useRoute
+	} from 'vue-router'
+	const router = useRouter()
+	const route = useRoute()
 
+	const pageKey = route.params.pageKey || '';
 	const name = 'rtTestRuntime';
-	
+
+	let updatedCmp = defineComponent({
+		template: '<div>test Me!</div>'
+	})
+	let c = h(updatedCmp);
+
 	function loadTest(file) {
-	  return defineAsyncComponent(() => import(`^/templates/${file}.vue`))
+		return defineAsyncComponent(() => import(`^/templates/${file}.vue`))
 	}
-	const c = loadTest(name);
+	//let c = loadTest(name);
 </script>
