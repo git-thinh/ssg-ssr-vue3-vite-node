5b14ed71ac654495f4dd20f2543f774d7e13cad6
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2022-09-22 22:18:53 +0700

    ok

--- v02/server/generate.js
+++ v02/server/generate.js
@@ -8,33 +8,27 @@ const root = process.cwd()
 const manifest = JSON.parse(fs.readFileSync(`${root}/${__outDir}/ssr-manifest.json`, 'utf8'))
 const template = fs.readFileSync(`${root}/${__outDir}/server/index.html`, 'utf-8')
 const {
-  render
+	render
 } = await import('../dist/server/entry-server.js')
 
-// determine routes to pre-render from src/pages
-const routesToPrerender = fs
-  .readdirSync(`${root}/src/pages`)
-  .map((file) => {
-    const name = file.replace(/\.vue$/, '').toLowerCase()
-    return name === 'home' ? `/` : `/${name}`
-  });
+const urls = ['/'];
 
 (async () => {
-  // pre-render each route...
-  for (const url of routesToPrerender) {
-    const [appHtml, preloadLinks] = await render(url, manifest)
+	// pre-render each route...
+	urls.forEach(async url => {
+		const [appHtml, preloadLinks] = await render(url, manifest)
 
-    const html = template
-      .replace(`<!--preload-links-->`, preloadLinks)
-      .replace(`<!--app-html-->`, appHtml)
+		const html = template
+			.replace(`<!--preload-links-->`, preloadLinks)
+			.replace(`<!--app-html-->`, appHtml)
 
-    const fileName = `${url === '/' ? '/index' : url}.html`;
-    const filePath = `${root}/${__outDir}${fileName}`
-    fs.writeFileSync(filePath, html)
+		const fileName = `${url === '/' ? '/index' : url}.html`;
+		const filePath = `${root}/${__outDir}${fileName}`
+		fs.writeFileSync(filePath, html)
 
-    console.log(`GEN -> ${fileName}`)
-  }
+		console.log(`GEN -> ${fileName}`)
+	});
 
-  // done, delete ssr manifest
-  //fs.unlinkSync(`${root}/${__outDir}/ssr-manifest.json`)
+	// done, delete ssr manifest
+	//fs.unlinkSync(`${root}/${__outDir}/ssr-manifest.json`)
 })()
